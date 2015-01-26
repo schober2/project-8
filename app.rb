e1a05c3f1892d346c52dd0f2327cad559a4f7206
Arthur Schober <schober2@msu.edu>
2015-01-26 12:06:05 -0500

    routed '/' to views/index.html

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require 'sinatra'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
-		"Welcome to the webapp of DOOM, where the copyright is always up to date. Copyright 2014-#{ Time.now.year } Arthur Schober."
+		send_file 'views/index.html'
 	end
 end
