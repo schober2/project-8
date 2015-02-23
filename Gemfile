1d472895d924224a821202ea8091a60beef04776
Arthur Schober <schober2@msu.edu>
2015-02-22 19:16:16 -0500

    added holidapi and layout file

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.1.5'
 
 gem 'sinatra', '~>1.4.5'
+gem 'holidapi'
--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 #app.rb
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
-                erb :"index#{ rand(3) + 1 }"
+    erb :"index#{ rand(3) + 1 }"
+	end
+	get '/holiday' do
+		time=Time.now
+    @holidays = HolidApi.get(country: 'US', year: time.year, month: time.month)
+    @holidays2 = HolidApi.get(country: 'US', year: 1988, month: 3)
+	  erb :holiday, :layout => :layoutsingle
 	end
 end
