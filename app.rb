412026656a8af09048bf5b843cb79119ccb306ba
User <user@deathstar.local>
2015-02-23 12:06:50 -0500

    added forms

--- app.rb
+++ app.rb
@@ -3,13 +3,19 @@ require 'sinatra'
 require 'holidapi'
 
 class MyWebApp < Sinatra::Base
-	get '/' do
+  get '/' do
     erb :"index#{ rand(3) + 1 }"
-	end
-	get '/holiday' do
-		time=Time.now
+  end
+  get '/holiday' do
+    time=Time.now
     @holidays = HolidApi.get(country: 'US', year: time.year, month: time.month)
     @holidays2 = HolidApi.get(country: 'US', year: 1988, month: 3)
-	  erb :holiday, :layout => :layoutsingle
-	end
+    erb :holiday, :layout => :layoutsingle
+  end
+  get '/holiday/select' do
+    erb :holidayselect, :layout => :layoutsingle
+  end
+  get '/holiday/selected' do
+    erb :holidayselected, :layout => :layoutsingle
+  end
 end
